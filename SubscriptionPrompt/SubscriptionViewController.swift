3cd96c5954a4945f12fd656cb3a0df0482903187
Binur <binur95@gmail.com>
2016-05-04 16:14:39 +0600

    restore button added. modalPresentationStyle set to .OverCurrentContext

--- SubscriptionPrompt/SubscriptionViewController.swift
+++ SubscriptionPrompt/SubscriptionViewController.swift
@@ -11,25 +11,39 @@ import SnapKit
 
 @objc public protocol SubscriptionViewControllerDelegate {
     func subscriptionViewControllerRowTapped(atIndex index: Int)
+    func restoreButtonTapped()
 }
 
 public class SubscriptionViewController: UIViewController, SubscribeViewDelegate {
     public var delegate: SubscriptionViewControllerDelegate?
     
     private var subscribeView: SubscribeView
+    private lazy var restorePurchasesButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = .systemFontOfSize(18)
+        button.addTarget(self, action: #selector(restoreButtonTapped),
+                         forControlEvents: .TouchUpInside)
+        return button
+    }()
     private var constraintsSetUp = false
     
     // MARK: - Init
     
     public init(title: String, images: [UIImage], commentTexts: [String], commentSubtitleTexts: [String],
-                     subscribeOptionsTexts: [String], cancelOptionText: String) {
+                subscribeOptionsTexts: [String], cancelOptionText: String, restoreButtonTitle: String) {
         subscribeView = SubscribeView(title: title, images: images,
                                       commentTexts: commentTexts,
                                       commentSubtitleTexts: commentSubtitleTexts,
                                       subscribeOptionsTexts: subscribeOptionsTexts,
                                       cancelOptionText: cancelOptionText)
         super.init(nibName: nil, bundle: nil)
+        
         subscribeView.delegate = self
+        restorePurchasesButton.setTitle(restoreButtonTitle, forState: .Normal)
+        
+        definesPresentationContext = true
+        providesPresentationContextTransitionStyle = true
+        modalPresentationStyle = .OverCurrentContext
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -40,7 +54,8 @@ public class SubscriptionViewController: UIViewController, SubscribeViewDelegate
     
     override public func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        [subscribeView, restorePurchasesButton].forEach { self.view.addSubview($0) }
         view.setNeedsUpdateConstraints()
     }
     
@@ -56,7 +71,12 @@ public class SubscriptionViewController: UIViewController, SubscribeViewDelegate
             $0.left.equalTo(view.snp_left).offset(20)
             $0.right.equalTo(view.snp_right).offset(-20)
             $0.top.equalTo(view.snp_top).offset(40)
-            $0.bottom.equalTo(view.snp_bottom).offset(-40)
+        }
+        restorePurchasesButton.snp_makeConstraints {
+            $0.left.equalTo(view.snp_left).offset(8)
+            $0.right.equalTo(view.snp_right).offset(-8)
+            $0.top.equalTo(subscribeView.snp_bottom).offset(20)
+            $0.bottom.lessThanOrEqualTo(view.snp_bottom)
         }
     }
     
@@ -68,10 +88,12 @@ public class SubscriptionViewController: UIViewController, SubscribeViewDelegate
     
     // MARK: - Private
     
-    private func setupViews() {
-        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        view.addSubview(subscribeView)
+    func restoreButtonTapped() {
+        guard let delegate = delegate else { return }
+        delegate.restoreButtonTapped()
     }
+    
+    // MARK: - SubscriptionViewDelegate
     
     func rowTapped(atIndex index: Int) {
         guard let delegate = delegate else { return }
